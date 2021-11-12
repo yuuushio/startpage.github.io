4fa0b2918c5dcea9760e884c63ed2e3612b8941e
Pi Al <piyushsayal3@gmail.com>
2021-11-12 20:22:21 +1100

    upload files after js integration

--- script.js
+++ script.js
@@ -1,64 +1,117 @@
-// Change this to your username
-var username = "grtcdr";
+/*
+ * Store current mode using localStorage
+ */
+if (localStorage.getItem("data") == null) var mode = "dark";
+else mode = localStorage.getItem("data");
 
-if (localStorage.getItem('data') == null) var mode = "dark";
-else mode = localStorage.getItem('data');
+/*
+ * Get time
+ */
+window.addEventListener("load", (event) => {
+  let today = new Date();
+  let time = today.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  change_colors(mode);
+  displayTime(time);
 
-// The same as "onload"
-window.addEventListener('load', (event) => {
-    let today = new Date();
-    let time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    change_colors(mode);
-    determineGreet(new Date().getHours());
-    displayTime(time);
+  //auto focus ddg-search upon page load
+  document.getElementById("input").focus();
 });
 
 setInterval(function () {
-    var today = new Date();
-    var time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    document.getElementById("time").innerHTML = time;
+  var today = new Date();
+  var time = today.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  document.getElementById("time").innerHTML = time;
 }, 1000);
 
+/*
+ * Change current mode upon icon click -- light/dark
+ */
 document.getElementById("mode").addEventListener("click", function () {
-    if (mode == "dark") {
-        change_colors("light");
-        localStorage.setItem('data', 'light');
-        data = localStorage.getItem('data');
-        mode = data;
-    }
-    else {
-        change_colors("dark");
-        localStorage.setItem('data', 'dark');
-        data = localStorage.getItem('data');
-        mode = data;
-    }
+  if (mode == "dark") {
+    change_colors("light");
+    localStorage.setItem("data", "light");
+    data = localStorage.getItem("data");
+    mode = data;
+  } else {
+    change_colors("dark");
+    localStorage.setItem("data", "dark");
+    data = localStorage.getItem("data");
+    mode = data;
+  }
 });
 
-function displayTime(time) {
-    document.getElementById("time").innerHTML = time;
-}
-
-
 function change_colors(mode) {
-    if (mode == "dark") light_colors();
-    else dark_colors();
+  if (mode == "dark") light_colors();
+  else dark_colors();
 }
 
+/*
+ * Output current time to screen
+ */
+function displayTime(time) {
+  document.getElementById("time").innerHTML = time;
+}
+
+/*
+ * Form/input variables for event listeners
+ */
 let input = document.getElementById("input");
+let input_g = document.getElementById("inputgoogle");
 let form = document.getElementById("search-bar");
-let indicator = document.getElementById("search-engine-indicator");
 
+/*
+ * Depending on the key pressed, focus on that input :
+ * 'd' to focus ddg input; 'g' to focus google search input.
+ */
+document.addEventListener("keypress", function (event) {
+  switch (event.key) {
+    // Focus on the search bar when pressing 's'
+    case "d":
+      if (
+        document.activeElement !== input &&
+        document.activeElement !== input_g
+      )
+        input.focus();
+      break;
+    // Switch to next search engine when pressing 'j'
+    case "g":
+      if (
+        document.activeElement !== input_g &&
+        document.activeElement !== input
+      )
+        input_g.focus();
+      break;
+    default:
+      return;
+  }
+});
 
-//search bar
-// Available search engines to cycle through
-let search_engines = ["Google", "DuckDuckGo", "Github", "StackOverflow", "Reddit"];
+/*
+ * Remove focus if esc is pressed
+ */
+document.addEventListener("keydown", function (event) {
+  if (event.key == "Escape") {
+    input.blur();
+    input_g.blur();
+  }
+});
 
-const MIN_SEARCH_ENGINE_INDEX = 0;
-const MAX_SEARCH_ENGINE_INDEX = search_engines.length - 1;
-let search_engine_index = 0;
+/*
+ * Submit form upon "enter"
+ */
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
 
-// Search function: Gets called on 'Enter' keypress
-// and depending on which search engine is currently selected
+/*
+ * DuckDuckGo Search the string input by the user.
+ */
 function duckduckgo_search(str) {
   if (str !== "") {
     search_query = "https://duckduckgo.com/?q=" + str;
@@ -66,8 +119,16 @@ function duckduckgo_search(str) {
   }
 }
 
-// Search function: Gets called on 'Enter' keypress
-// and depending on which search engine is currently selected
+input.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    duckduckgo_search(input.value);
+    input.blur();
+  }
+});
+
+/*
+ * Google Search the string input by the user.
+ */
 function google_search(str) {
   if (str !== "") {
     search_query = "https://www.google.com/search?q=" + str;
@@ -75,110 +136,92 @@ function google_search(str) {
   }
 }
 
-
-// ------D D G------
-// Search function: Gets called on 'Enter' keypress
-// and depending on which search engine is currently selected
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-});
-
-let ddg_search = document.getElementById("input");
-ddg_search.addEventListener("keypress", function (event) {
-  if (event.key == 'Enter') {
-    duckduckgo_search(ddg_search.value);
-    ddg_search.blur();
+input_g.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    google_search(input_g.value);
+    input_g.blur();
   }
 });
 
-//------- GOOGLE-------
-// Search function: Gets called on 'Enter' keypress
-// and depending on which search engine is currently selected
-
-
-let g_search = document.getElementById("inputgoogle");
-g_search.addEventListener("keypress", function (event) {
-  if (event.key == 'Enter') {
-    google_search(g_search.value);
-    g_search.blur();
-  }
-});
-
-
-
-
-
+/*
+ * Mode specific css styling
+*/
 function dark_colors() {
-    let link = document.getElementsByTagName('a');
-    let mode_id = document.getElementById("mode");
-    document.body.style.background = "#2E3440";
-    mode_id.src = "ico/light.png";
-    mode_id.style.filter = "invert(100%) sepia(0%) saturate(1620%) hue-rotate(8deg) brightness(94%) contrast(88%)";
-    for (i = 0; i < link.length; i++) 
-        link[i].style.color = "rgba(216,222,233,0.3)";
-        
-    
-	let time_id = document.getElementById("time");
-    time_id.style.color = "#d8dee9";
-    let input_i = document.getElementById("input");
-    input_i.style.background = "#2E3440";
-    //input_i.style.boxShadow = "rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px";
-    /*input_i.onmouseout = () =>{
-    	input_i.style.boxShadow="rgba(0, 0, 0, 0.1) 1.95px 1.95px 2.6px";
-    }
-    input_i.onmouseover = () =>{
-    	input_i.style.boxShadow="inset 3px 3px 6px #272c36, inset -3px -3px 6px #353c4a";
-    }*/
-    
-    let input_g = document.getElementById("inputgoogle");
-   
-    input_g.style.background = "#2E3440";
-    //input_g.style.boxShadow = "rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px";
-    
-    
-	let list_s = document.getElementById("ls"); 
-	/*list_s.style.border="2px solid rgba(59, 66, 82, 0.5)"; 
-	list_s.style.borderRadius="8px"; */
-	list_s.style.background="#2E3440";
+  let link = document.getElementsByTagName("a");
+  let mode_id = document.getElementById("mode");
+  document.body.style.background = "#242933";
+  mode_id.src = "ico/light.png";
+  mode_id.style.filter =
+    "invert(100%) sepia(18%) saturate(3654%) hue-rotate(177deg) brightness(100%) contrast(84%)";
+
+  let img_id = document.getElementById("ac");
+  img_id.src = "ico/ac1.png";
+  img_id.height = "300";
+
+  let img_container = document.getElementsByClassName("gif");
+  img_container[0].style.border = "2px solid #81a1c1";
+  img_container[0].style.borderRadius = "5px";
+  img_container[0].style.boxShadow =
+    "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px";
+
+  let main_cont = document.getElementsByClassName("container");
+  main_cont[0].style.background = "#242933";
+
+  for (i = 0; i < link.length; i++) link[i].style.color = "#81a1c1";
+
+  let time_id = document.getElementById("time");
+  time_id.style.color = "#81a1c1";
+  let input_i = document.getElementById("input");
+  input_i.style.background = "rgba(59, 66, 82, 0.1)";
+  input_i.style.borderBottom = "2px solid #81a1c1";
+  let input_g = document.getElementById("inputgoogle");
+
+  input_g.style.background = "rgba(59, 66, 82, 0.1)";
+  input_g.style.borderBottom = "2px solid #81a1c1";
+
+  let list_s = document.getElementById("ls");
+
+  let time_i = document.getElementById("time");
+  time_i.style.borderRadius = "4px";
+  time_i.style.boxShadow = "none";
+  time_i.style.background = "none";
 }
 
 function light_colors() {
-    let link = document.getElementsByTagName('a');
-    let mode_id = document.getElementById("mode");
-    document.body.style.background = "#E5E9F0";
-    mode_id.src = "ico/dark.png";
-    mode_id.style.filter = "invert(58%) sepia(12%) saturate(482%) hue-rotate(148deg) brightness(94%) contrast(86%)";
-    for (i = 0; i < link.length; i++) 
-        link[i].style.color = "rgba(76, 86, 106, 0.7)";
-    let time_id = document.getElementById("time");
-    time_id.style.color = "#4C566A";
-    let input_i = document.getElementById("input");
-    input_i.style.background = "#E5E9F0";
-    input_i.style.borderRadius = "4px";
-    //input_i.style.boxShadow="rgba(0, 0, 0, 0.1) 1.95px 1.95px 2.6px";
-    /*input_i.onmouseout = () =>{
-    	input_i.style.boxShadow="rgba(0, 0, 0, 0.1) 1.95px 1.95px 2.6px";
-    }
-    input_i.onmouseover = () =>{
-    	input_i.style.boxShadow="inset 5px 5px 7px #dadde4, inset -5px -5px 7px #f0f5fc";
-    }*/
-    
-    
-    let input_g = document.getElementById("inputgoogle");
-    input_g.style.background = "#E5E9F0";
-    
-    //input_g.style.boxShadow="rgba(0, 0, 0, 0.1) 1.95px 1.95px 2.6px";
-    /*input_g.onmouseout = () =>{
-    	input_g.style.boxShadow="rgba(0, 0, 0, 0.1) 1.95px 1.95px 2.6px";
-    }
-    input_g.onmouseover = () =>{
-    	input.style.boxShadow="inset 5px 5px 7px #dadde4, inset -5px -5px 7px #f0f5fc";
-    }*/
-    
-    let list_s = document.getElementById("ls");
-   /* list_s.style.border="1px solid rgba(236, 239, 244, 0.8)";
-    list_s.style.borderRadius="8px";*/
-    list_s.style.background="#E5E9F0";
-    
-}
+  let link = document.getElementsByTagName("a");
+  let mode_id = document.getElementById("mode");
+  document.body.style.background = "#E5E9F0";
+  mode_id.src = "ico/moon.png";
+  mode_id.style.filter =
+    "invert(20%) sepia(21%) saturate(481%) hue-rotate(181deg) brightness(89%) contrast(94%)";
 
+  let img_id = document.getElementById("ac");
+  img_id.src = "ico/ac.png";
+  img_id.height = "250";
+
+  let main_cont = document.getElementsByClassName("container");
+  main_cont[0].style.background = "#E5E9F0";
+
+  let img_container = document.getElementsByClassName("gif");
+  img_container[0].style.border = "0px";
+  img_container[0].style.boxShadow = "none";
+
+  for (i = 0; i < link.length; i++)
+    link[i].style.color = "rgba(59, 66, 82, 0.9)";
+  let time_id = document.getElementById("time");
+  time_id.style.color = "rgba(59, 66, 82, 0.9)";
+  let input_i = document.getElementById("input");
+  input_i.style.background = "rgba(216,222,233,0.2)";
+  input_i.style.borderRadius = "4px";
+  input_i.style.borderBottom = "2px solid rgba(59, 66, 82, 0.7)";
+  let input_g = document.getElementById("inputgoogle");
+  input_g.style.background = "rgba(216,222,233,0.2)";
+  input_g.style.borderBottom = "2px solid rgba(59, 66, 82, 0.7)";
+
+  let list_s = document.getElementById("ls");
+
+  let time_i = document.getElementById("time");
+  time_i.style.borderRadius = "4px";
+  time_i.style.background = "#E5E9F0";
+  time_i.style.boxShadow = "3px 3px 11px #d5d9df,-3px -3px 11px #f5f9ff";
+}
